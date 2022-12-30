6a11aa86db42d6d771b2ef3eabc441361713d5b7
Michelle Lo <lomichelle42@gmail.com>
2022-12-30 13:18:47 +0800

    Complete log in and log out authentication features.

--- app/src/main/java/com/example/funnel/HomeActivity.java
+++ app/src/main/java/com/example/funnel/HomeActivity.java
@@ -1,8 +1,16 @@
 package com.example.funnel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +18,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button logoutButton = findViewById(R.id.logout_button);
+        logoutButton.setOnClickListener(view -> AuthUI.getInstance()
+                .signOut(HomeActivity.this)
+                .addOnCompleteListener(task -> {
+                    Intent loginIntent = new Intent(HomeActivity.this, MainActivity.class);
+                    startActivity(loginIntent);
+                }));
     }
 }
